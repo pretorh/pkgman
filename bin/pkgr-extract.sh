e20f631b041615c3aa98bfccbb73a1f6a8d3ae66
Hendri Pretorius <pretorh@gmail.com>
2022-05-08 17:54:08 +0200

    extract: print root, extracted file names

--- bin/pkgr-extract.sh
+++ bin/pkgr-extract.sh
@@ -5,4 +5,5 @@ if [ "$PKGR_EXTRACT_ROOT" == "" ] && [ "$EUID" == 0 ] ; then
   PKGR_EXTRACT_ROOT=/
 fi
 
-tar -xvPJf "$1" --strip-components=1 --directory="$PKGR_EXTRACT_ROOT" --no-overwrite-dir
+echo "extracting $1 to $PKGR_EXTRACT_ROOT"
+tar -xvPJf "$1" --strip-components=1 --show-transformed-names --directory="$PKGR_EXTRACT_ROOT" --no-overwrite-dir
