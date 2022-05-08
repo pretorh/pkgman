fbea1b40a0217361253f48791c8d98dfd374e622
Hendri Pretorius <pretorh@gmail.com>
2022-05-08 15:15:52 +0200

    tests: add util to create test tar with given files

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+TEST_ROOT=$(mktemp --dir)
+
 fail() {
   echo "Fail: $1" >&2
   caller >&2
@@ -18,4 +20,18 @@ assert_not_exists() {
       fail "'$f' does exist"
     fi
   done
+}
+
+create_test_tar() {
+  data="$(mktemp --directory --tmpdir="$TEST_ROOT" XXXXX)"
+  mkdir -p "$data/root"
+
+  for v in "$@" ; do
+    f="$data/root/$v"
+    mkdir -p "$(dirname "$f")"
+    echo "v" > "$f"
+  done
+
+  tar -cJf "$data/files.tar.xz" --directory="$data" root || fail "failed to create tar of $data"
+  echo "$data/files.tar.xz"
 }
