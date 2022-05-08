5a84aa13845f9df5376a0887a7f4ca6de3747c80
Hendri Pretorius <hendri@pretorh.xyz>
2022-05-08 17:49:28 +0200

    extract: do not overwrite directory metadata on extract

--- bin/pkgr-extract.sh
+++ bin/pkgr-extract.sh
@@ -5,4 +5,4 @@ if [ "$PKGR_EXTRACT_ROOT" == "" ] && [ "$EUID" == 0 ] ; then
   PKGR_EXTRACT_ROOT=/
 fi
 
-tar -xvPJf "$1" --strip-components=1 --directory="$PKGR_EXTRACT_ROOT"
+tar -xvPJf "$1" --strip-components=1 --directory="$PKGR_EXTRACT_ROOT" --no-overwrite-dir
--- tests/sudo.sh
+++ tests/sudo.sh
@@ -8,5 +8,10 @@ cd "$(dirname "$0")/.."
 name=$(uuidgen)
 f=$(create_test_tar "tmp/$name/a")
 PKGR_EXTRACT_ROOT="" bin/pkgr-extract.sh "$f" 2>"$TEST_ROOT/stderr"
+
+# files created
 assert_exists / "tmp/$name/a"
+# no warnings
 assert_empty_file "$TEST_ROOT/stderr"
+# keep metadata
+stat -c '%a' "/tmp" | assert_piped "1777"
