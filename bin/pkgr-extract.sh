30739ede57d5317080e78dbbaa1b76c52e07cd2c
Hendri Pretorius <pretorh@gmail.com>
2022-05-08 17:22:54 +0200

    tests: extract to / when running as root user
    - add test for testing workings when running as root
    - extract to / when PKGR_EXTRACT_ROOT is not set and running as root

--- bin/pkgr-extract.sh
+++ bin/pkgr-extract.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+if [ "$PKGR_EXTRACT_ROOT" == "" ] && [ "$EUID" == 0 ] ; then
+  PKGR_EXTRACT_ROOT=/
+fi
+
 tar -xvPJf "$1" --strip-components=1 --directory="$PKGR_EXTRACT_ROOT"
