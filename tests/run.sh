42ca2582acba1c8927f76440397e969878781a0e
Hendri Pretorius <hendri@pretorh.xyz>
2022-05-14 19:17:17 +0200

    tests: log skipped tests

--- tests/run.sh
+++ tests/run.sh
@@ -3,17 +3,19 @@ set -e
 cd "$(dirname "$0")/../tests/"
 
 failed=0
+skipped=0
 for test_file in **/*.sh ; do
   echo "# $test_file"
   if bash -e "$test_file" >/dev/null ; then
     echo "ok $test_file"
   elif [ $? = 5 ] ; then
     echo "not ok $test_file # TODO exited with 5"
+    skipped=$((skipped+1))
   else
     echo "not ok $test_file"
     failed=$((failed+1))
   fi
 done
 
-echo "$failed failed"
+echo "$failed failed ($skipped skipped)"
 test $failed -eq 0
