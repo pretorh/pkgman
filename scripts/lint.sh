ae42bb858ea855be5663c7757efb66023224c4d4
Hendri Pretorius <hendri@pretorh.xyz>
2022-05-14 20:02:41 +0200

    ci: ensure lint is in run from root

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 set -e
+cd "$(dirname "$0")/.."
 
 curl -L --silent --output shellcheck.tar.xz https://github.com/koalaman/shellcheck/releases/download/stable/shellcheck-stable.linux.x86_64.tar.xz
 tar -xf shellcheck.tar.xz shellcheck-stable/shellcheck --strip-components 1
