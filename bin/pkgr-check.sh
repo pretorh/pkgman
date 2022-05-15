46113ecc95d7c96f1cd091804f9a514d413b43cf
Hendri Pretorius <hendri@pretorh.xyz>
2022-05-15 09:07:39 +0200

    check: simplify tar command
    extracted dir not needed

--- bin/pkgr-check.sh
+++ bin/pkgr-check.sh
@@ -2,5 +2,5 @@
 set -e
 libexec="$(dirname "$0")/../libexec"
 
-tar --list -Pf "$1" --strip-components=1 --show-transformed-names --directory="$PKGR_EXTRACT_ROOT" \
+tar --list -Pf "$1" --strip-components=1 --show-transformed-names \
   | xargs -n1 sh "$libexec/"check-exists.sh
