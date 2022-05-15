ffaf052b204f032aa388b0965c593403a011228d
Hendri Pretorius <hendri@pretorh.xyz>
2022-05-15 08:48:32 +0200

    check: list files that exist on filesystem

--- bin/pkgr-check.sh
+++ bin/pkgr-check.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
 set -e
+libexec="$(dirname "$0")/../libexec"
+
+tar --list -Pf "$1" --strip-components=1 --show-transformed-names --directory="$PKGR_EXTRACT_ROOT" \
+  | grep -v '/$' \
+  | xargs -n1 sh "$libexec/"check-exists.sh
