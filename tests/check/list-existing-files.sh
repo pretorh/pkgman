8f46ca30071e28942f1078e37465a26fcd021743
Hendri Pretorius <hendri@pretorh.xyz>
2022-05-15 08:57:35 +0200

    check: add test for showing existing symlinks

--- tests/check/list-existing-files.sh
+++ tests/check/list-existing-files.sh
@@ -7,16 +7,22 @@ cd "$(dirname "$0")/../.."
 f=$(create_test_tar "a" "b" "c/d" "c/e/f")
 
 # when some files already exist
-mkdir "$PKGR_EXTRACT_ROOT/c"
+mkdir -p "$PKGR_EXTRACT_ROOT/c/e"
 touch "$PKGR_EXTRACT_ROOT/a"
 touch "$PKGR_EXTRACT_ROOT/c/d"
+ln -sv "$PKGR_EXTRACT_ROOT/c/d" "$PKGR_EXTRACT_ROOT/c/e/f"
 
-# fail with list of files
+# exit with failure
 if ./bin/pkgr-check.sh "$f" 2>/dev/null 1>"$TEST_ROOT/stdout" ; then
   fail "expected check to fail"
 fi
+
+# with list of files in output
 assert_grep "/a" "$TEST_ROOT/stdout"
 assert_grep "/c/d" "$TEST_ROOT/stdout"
+
+# with symlinks in output
+assert_grep "/c/e/f" "$TEST_ROOT/stdout"
 
 # does not list directories
 grep "/c/$" "$TEST_ROOT/stdout" && fail "should not include directories in output"
