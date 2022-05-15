46b2847b4454ac7fb018239bdb18ef63f92af31a
Hendri Pretorius <hendri@pretorh.xyz>
2022-05-15 17:43:51 +0200

    remove: add message on remove, update README

--- bin/pkgr-remove.sh
+++ bin/pkgr-remove.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
+echo "remove files in $1 from $PKGR_EXTRACT_ROOT"
 tar --list -Pf "$1" --strip-components=1 --show-transformed-names \
   | grep -v '/$' \
   | xargs -IFILE rm -vf "$PKGR_EXTRACT_ROOT/FILE"
