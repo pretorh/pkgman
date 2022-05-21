158c60226219a899dd07e3bc3b7b5908ba34a5e8
Hendri Pretorius <hendri@pretorh.xyz>
2022-05-21 15:44:30 +0200

    extract: create a temp exclude file if not present
    and combine tar command into a single line

--- bin/pkgr-extract.sh
+++ bin/pkgr-extract.sh
@@ -6,8 +6,5 @@ if [ "$PKGR_EXTRACT_ROOT" == "" ] && [ "$EUID" == 0 ] ; then
 fi
 
 echo "extracting $1 to $PKGR_EXTRACT_ROOT"
-if [ "$2" != "" ] ; then
-  tar -xvPJf "$1" --strip-components=1 --show-transformed-names --directory="$PKGR_EXTRACT_ROOT" --no-overwrite-dir --exclude-from="$2"
-  exit 0
-fi
-tar -xvPJf "$1" --strip-components=1 --show-transformed-names --directory="$PKGR_EXTRACT_ROOT" --no-overwrite-dir
+exclude=${2-$(mktemp)}
+tar -xvPJf "$1" --strip-components=1 --show-transformed-names --directory="$PKGR_EXTRACT_ROOT" --no-overwrite-dir --exclude-from="$exclude"
