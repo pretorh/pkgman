768f2205c9d0549186493f4339bf12757c81f934
Hendri Pretorius <hendri@pretorh.xyz>
2022-05-21 16:26:10 +0200

    extract: move getting excludes file up
    get before logging, log just before extracting

--- bin/pkgr-extract.sh
+++ bin/pkgr-extract.sh
@@ -5,6 +5,7 @@ if [ "$PKGR_EXTRACT_ROOT" == "" ] && [ "$EUID" == 0 ] ; then
   PKGR_EXTRACT_ROOT=/
 fi
 
-echo "extracting $1 to $PKGR_EXTRACT_ROOT"
 exclude=${2-$(mktemp)}
+
+echo "extracting $1 to $PKGR_EXTRACT_ROOT"
 tar -xvPJf "$1" --strip-components=1 --show-transformed-names --directory="$PKGR_EXTRACT_ROOT" --no-overwrite-dir --exclude-from="$exclude"
