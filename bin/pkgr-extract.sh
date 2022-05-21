cc21d26dc28c6526ba9fd47c8ebbd3976723efff
Hendri Pretorius <hendri@pretorh.xyz>
2022-05-21 15:37:52 +0200

    extract: allow skipping files to extract

--- bin/pkgr-extract.sh
+++ bin/pkgr-extract.sh
@@ -6,4 +6,8 @@ if [ "$PKGR_EXTRACT_ROOT" == "" ] && [ "$EUID" == 0 ] ; then
 fi
 
 echo "extracting $1 to $PKGR_EXTRACT_ROOT"
+if [ "$2" != "" ] ; then
+  tar -xvPJf "$1" --strip-components=1 --show-transformed-names --directory="$PKGR_EXTRACT_ROOT" --no-overwrite-dir --exclude-from="$2"
+  exit 0
+fi
 tar -xvPJf "$1" --strip-components=1 --show-transformed-names --directory="$PKGR_EXTRACT_ROOT" --no-overwrite-dir
