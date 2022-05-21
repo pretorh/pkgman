88d6fad7c932bacc320fc9deadd5a494983a527b
Hendri Pretorius <hendri@pretorh.xyz>
2022-05-21 15:51:12 +0200

    check: do not check for excluded files

---
+++ tests/check/ignore-excluded.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+cd "$(dirname "$0")/../.."
+. tests/setup.sh
+
+# a package
+f=$(create_test_tar "a" "b" "c/d" "c/e/f")
+
+# when files already exist
+touch "$PKGR_EXTRACT_ROOT/a"
+
+# but they are ignored
+x=$(create_exclude_file "a")
+
+# does not match, no output
+./bin/pkgr-check.sh "$f" "$x" 2>/dev/null 1>"$TEST_ROOT/stdout" || fail "check failed with exit code $?"
+assert_empty_file "$TEST_ROOT/stdout"
+
+# when unignored files exists, it should still fail and list them in output
+touch "$PKGR_EXTRACT_ROOT/b"
+if ./bin/pkgr-check.sh "$f" "$x" 2>/dev/null 1>"$TEST_ROOT/stdout" ; then
+  fail "expected check to fail"
+fi
+assert_grep "/b" "$TEST_ROOT/stdout"
