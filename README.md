d10a498e287275c2db921ea57897bbcbe998ae10
Hendri Pretorius <pretorh@gmail.com>
2024-09-01 15:04:46 +0200

    lint: use system shellcheck for linting

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ So far this is just a convenience wrapper around `tar` commands:
 - extracting files
 - checking for existing files
 - removing files
+
+## development
+
+### requirement
+
+- `shellcheck`
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,9 +2,7 @@
 set -e
 cd "$(dirname "$0")/.."
 
-curl -L --silent --output shellcheck.tar.xz https://github.com/koalaman/shellcheck/releases/download/stable/shellcheck-stable.linux.x86_64.tar.xz
-tar -xf shellcheck.tar.xz shellcheck-stable/shellcheck --strip-components 1
-find . -name '*.sh' -print0 | xargs -0 ./shellcheck -f gcc
+find . -name '*.sh' -print0 | xargs -0 shellcheck -f gcc
 
 if grep '#!/usr/bin/env ' bin/* libexec/* | grep -v bash ; then
   echo "all scripts should run as bash" >&2
